ca0242dab1f05ffa759edac6c3fbf7f9637f4b1e
mikebailey-ga <mikeyobviously@gmail.com>
2019-06-11 17:30:24 -0700

    end of day

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Shoe(models.Model):
+    name = models.CharField(max_length=40)
+    brand = models.CharField(max_length=40)
+    color = models.CharField(max_length=40)    
+    description = models.TextField(max_length=100)    
+    category = models.CharField(max_length=40)    
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Shoe
 # Create your views here.
 
-class Shoe:
-    def __init__(self, name, brand, description, kind):
-        self.name = name
-        self.brand = brand
-        self.description = description
-        self.kind = kind
-
-shoes = [
-    Shoe('Indy 405', 'Alden', 'Indy shoes', 'work boot'),
-    Shoe('Jordan 3', 'Nike', 'Black Jordan 3s', 'basketball'),
-    Shoe('Cap toe brown boot', 'Allen Edmonds', 'Cap toe brown boot', 'boot')
-]
 
 def home(request):
-    return HttpResponse('<h1>Test</h1>')
+    return render(request,'index.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def shoes_index(request):
+    shoes = Shoe.objects.all()
     return render(request, 'shoes/index.html', {'shoes': shoes})
